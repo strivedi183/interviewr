f68202c591a9647ab5056ddf26446aba1e83b9d4
Eric Holland <eric@f-robots.com>
2013-04-04 13:47:18 -0400

    built answer and question factories and model tests

---
+++ spec/factories/answer_factory.rb
@@ -0,0 +1,5 @@
+FactoryGirl.define do
+  factory :answer, class: Answer do
+    name                  'Ruby on Rails'
+  end
+end
---
+++ spec/factories/question_factory.rb
@@ -0,0 +1,5 @@
+FactoryGirl.define do
+  factory :question, class: Question do
+    prompt                 'What is the best coding language'
+  end
+end
