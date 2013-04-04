1d2cd275bd4eb912535aad1b11ea6b75737cda45
Sandip Trivedi <trivedi.sandip@gmail.com>
2013-04-04 11:47:41 -0400

    Changed test to quiz in the model

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,5 @@
 class Question < ActiveRecord::Base
-  attr_accessible :prompt, :test_id
+  attr_accessible :prompt, :quiz_id
   belongs_to :test, :inverse_of => :questions
   has_many :answers, :inverse_of => :question
 end
--- app/models/test.rb
+++
@@ -1,8 +0,0 @@
-class Test < ActiveRecord::Base
-  attr_accessible :name, :is_public, :cost, :code, :pass, :user_id
-  has_many :results, :inverse_of => :test
-  has_many :questions, :inverse_of => :test
-  belongs_to :user, :inverse_of => :tests
-  has_and_belongs_to_many :tags
-
-end
