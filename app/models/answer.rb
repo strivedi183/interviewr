15292662b0b63cb8efe48cb2d4961124187527b9
Sandip Trivedi <trivedi.sandip@gmail.com>
2013-04-04 12:06:28 -0400

    Fixed models and migration files

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: answers
+#
+#  id          :integer          not null, primary key
+#  name        :string(255)
+#  is_correct  :boolean
+#  question_id :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class Answer < ActiveRecord::Base
   attr_accessible :name, :is_correct, :question_id
   belongs_to :question, :inverse_of => :answers
--- app/models/result.rb
+++ app/models/result.rb
@@ -1,5 +1,17 @@
+# == Schema Information
+#
+# Table name: results
+#
+#  id          :integer          not null, primary key
+#  user_id     :integer
+#  quiz_id     :integer
+#  num_correct :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class Result < ActiveRecord::Base
   attr_accessible :user_id, :quiz_id, :num_correct
   belongs_to :user, :inverse_of => :results
-  belongs_to :test, :inverse_of => :results
+  belongs_to :quiz, :inverse_of => :results
 end
