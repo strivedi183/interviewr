639ba02b67b583721fd5778f6e3a99cab9d601e5
Sandip Trivedi <trivedi.sandip@gmail.com>
2013-04-04 13:27:03 -0400

    Added factories for the models

---
+++ spec/factories/tag_factory.rb
@@ -0,0 +1,15 @@
+# == Schema Information
+#
+# Table name: tags
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+FactoryGirl.define do
+  factory :tag, class: Tag do
+    name                  'tag 1'
+  end
+end
