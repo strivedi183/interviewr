61653f4fff33574fcf9869cb41d413038126000b
Sandip Trivedi <trivedi.sandip@gmail.com>
2013-04-04 17:00:31 -0400

    Added to the seed file some results

--- db/seeds.rb
+++ db/seeds.rb
@@ -32,3 +32,36 @@ a12 = Answer.create(:name => 'Choice 4', :is_correct => true)
 question3.answers << a9 << a10 << a11 << a12
 q1.questions << question1 << question2 << question3
 # End Block
+
+# This block is to set up a sample quiz
+q2 = Quiz.create(:name => 'Quiz 2', :num_pass => 2)
+u2.quizzes << q2
+question4 = Question.create(:prompt => 'This is Question 1')
+a13 = Answer.create(:name => 'Choice 1', :is_correct => true)
+a14 = Answer.create(:name => 'Choice 2', :is_correct => false)
+a15 = Answer.create(:name => 'Choice 3', :is_correct => true)
+a16 = Answer.create(:name => 'Choice 4', :is_correct => false)
+question4.answers << a13 << a14 << a15 << a16
+question5 = Question.create(:prompt => 'This is Question 2')
+a17 = Answer.create(:name => 'Choice 1', :is_correct => false)
+a18 = Answer.create(:name => 'Choice 2', :is_correct => false)
+a19 = Answer.create(:name => 'Choice 3', :is_correct => true)
+a20 = Answer.create(:name => 'Choice 4', :is_correct => true)
+question5.answers << a17 << a18 << a19 << a20
+question6 = Question.create(:prompt => 'This is Question 3')
+a21 = Answer.create(:name => 'Choice 1', :is_correct => true)
+a22 = Answer.create(:name => 'Choice 2', :is_correct => false)
+a23 = Answer.create(:name => 'Choice 3', :is_correct => false)
+a24 = Answer.create(:name => 'Choice 4', :is_correct => true)
+question6.answers << a21 << a22 << a23 << a24
+q2.questions << question4 << question5 << question6
+# End Block
+
+#
+r1 = Result.create(:quiz_id => q1.id, :user_id => u2.id, :num_correct => 1)
+r2 = Result.create(:quiz_id => q1.id, :user_id => u2.id, :num_correct => 2)
+r3 = Result.create(:quiz_id => q1.id, :user_id => u2.id, :num_correct => 3)
+r4 = Result.create(:quiz_id => q2.id, :user_id => u2.id, :num_correct => 1)
+r5 = Result.create(:quiz_id => q2.id, :user_id => u2.id, :num_correct => 2)
+r6 = Result.create(:quiz_id => q2.id, :user_id => u2.id, :num_correct => 3)
+#
