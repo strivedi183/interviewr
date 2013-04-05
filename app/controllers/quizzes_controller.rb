fe76f8e175252bbb564b92fd589781022a3856f7
Ron Jones <rrrron@gmail.com>
2013-04-05 09:39:35 -0400

    nav to create qz, additions

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -11,5 +11,14 @@ class QuizzesController < ApplicationController
   end
 
   def create
+    @quiz = Quiz.create(params[:quiz])
+    tags = params[:tags].split(',')
+    tags.each do |tag|
+      tag = tag.squish
+      t = Tag.where(name: tag).first
+      t = Tag.new(name: tag) if t.nil?
+      @quiz.tags << t
+    end
+    redirect_to root_path
   end
 end
