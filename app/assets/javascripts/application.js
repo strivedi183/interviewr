fcc14ffb842dc4917717ad249220c5c327e3308c
Sandip Trivedi <trivedi.sandip@gmail.com>
2013-04-06 15:16:11 -0400

    Added charts to the quiz analytics

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,6 +3,8 @@
 //= require jquery.remotipart
 //= require 'foundation-4/js/vendor/custom.modernizr.js'
 //= require 'foundation-4/js/foundation.min.js'
+//= require 'morris/spec/vendor/raphael-2.1.0.min.js'
+//= require 'morris/morris.min.js'
 //= require 'underscore.js'
 //= require 'sugar.js'
 //= require 'd3.v3.js'
